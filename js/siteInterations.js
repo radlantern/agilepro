55ecfcaedb3585b9b9e2650de1efae6d09ff18d4
Robert Lynch <rcl@agilepro.com>
2014-04-15 19:43:02 -0700

    awesome, just awesome

--- js/siteInterations.js
+++ js/siteInterations.js
@@ -3,6 +3,13 @@ function getRandomInt (min, max) {
 }
 $(document).ready(function()
 {
+	//H8Rs be H8n
+	$('#siteName').html('<div><span>'+$('#siteName').text().split("").join('</span></div><div><span>')+'</span></div>').find('div').each(function(index){$(this).attr('id','nameLetter'+index)});
+
+	$('#siteName div').each(function(index){
+		TweenMax.to(this,0.3,{css:{z: -100}, delay: 0.1 * index});
+		TweenMax.to(this,0.3,{css:{z: 0}, delay: 0.1 * index + 0.3});
+	});
 	//Perform the intro animation then load the
 	TweenMax.fromTo('#nucleus',2,{rotationY: getRandomInt(-500,500), rotationX: getRandomInt(-500,500), z: 5000},{rotationY: 230, rotationX: 140, z:0, onComplete: logoInteractions});
 	TweenMax.to('.face',0.75,{opacity: 0.8});
@@ -10,13 +17,14 @@ $(document).ready(function()
 
 function logoInteractions() {
 	danceLoop();
-	$('#nucleus').click(function(){
-		if(window.wheeeTl && window.wheeeTl._active)
-		{
-			return;
-		}
+
+
+
+	//For Jason
+	$('#logo').css({cursor: 'help'}).click(function(){
+		if(window.wheeeTl && window.wheeeTl._active) return;
 		danceLoop(true);
-		var tl = window.wheeeTl = new TimelineMax({repeat:0, onComplete: danceLoop}); //alert('done'); }
+		var tl = window.wheeeTl = new TimelineMax({repeat:0, onComplete: danceLoop});
 		tl.add(TweenMax.to('#nucleus',3,{rotationY: 3000}));
 		tl.add(TweenMax.to('#nucleus',3,{rotationY: 230, rotationX: 140, ease: Sine.easeInOut}));
 		TweenMax.to('#wheee',0.5,{css:{opacity: 1}});
@@ -42,8 +50,8 @@ function danceLoop(stop) {
 		return;
 	}
 	tl = window.danceTl = new TimelineMax({repeat:0});
-	tl.add(TweenMax.to('#nucleus.dance',2,{rotationY: getRandomInt(-500,500), rotationX: getRandomInt(-500,500)}));
-	tl.add(TweenMax.to('#nucleus.dance',2,{rotationY: getRandomInt(-500,500), rotationX: getRandomInt(-500,500)}));
-	tl.add(TweenMax.to('#nucleus.dance',2,{rotationY: 590, rotationX: 500,}));
+	tl.add(TweenMax.to('#nucleus.dance',2,{rotationY: getRandomInt(-360,360), rotationX: getRandomInt(-360,360)}));
+	tl.add(TweenMax.to('#nucleus.dance',2,{rotationY: getRandomInt(-360,360), rotationX: getRandomInt(-360,360)}));
+	tl.add(TweenMax.to('#nucleus.dance',2,{rotationY: 230, rotationX: 140,}));
 	window.danceLoopTimer = setTimeout(danceLoop, 15000);
 }
